ecebf3e627fa5c962e08bb2abc81878f935f5bbe
Cranberis <cranberis@yandex.ru>
2025-07-30 18:39:48 +0300

    feat(cmp): объединена конфигурация Lazy + LSP + LuaSnip
    - Объединены старые и новые настройки cmp
    - Поддержка LuaSnip и командной строки
    - Добавлен gitcommit source
    - Полная совместимость с Lazy.nvim

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,67 +1,109 @@
+-- Конфигурация плагина автодополнения nvim-cmp
 
+return {
+  "hrsh7th/nvim-cmp",           -- сам плагин автодополнения
 
-local cmp = require 'cmp'
+  event = "InsertEnter",        -- загружать при входе в режим вставки
 
-cmp.setup({
-    snippet = {
-        -- REQUIRED - you must specify a snippet engine
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",         -- поддержка автодополнения от LSP
+    "hrsh7th/cmp-buffer",           -- предложения из текущего буфера
+    "hrsh7th/cmp-path",             -- автодополнение путей к файлам
+    "hrsh7th/cmp-cmdline",          -- автодополнение в командной строке
+    "hrsh7th/cmp-git",              -- автодополнение для коммитов Git
+    "saadparwaiz1/cmp_luasnip",     -- сниппеты от LuaSnip
+    "L3MON4D3/LuaSnip",             -- сам движок сниппетов
+  },
+
+  config = function()
+    local cmp = require("cmp")            -- сам модуль автодополнения
+    local luasnip = require("luasnip")    -- модуль сниппетов
+
+    -- Включаем автодополнение с поддержкой сниппетов
+    cmp.setup({
+      snippet = {
         expand = function(args)
-            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-        end
+          luasnip.lsp_expand(args.body)   -- используем LuaSnip для вставки сниппетов
+        end,
+      },
 
-    },
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered()
-    },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({select = true}),
+      window = {
+        completion = cmp.config.window.bordered(),     -- границы окна предложений
+        documentation = cmp.config.window.bordered(),  -- границы окна документации
+      },
+
+      formatting = {
+        fields = { "abbr", "menu", "kind" },  -- порядок отображаемых полей
+      },
+
+      mapping = cmp.mapping.preset.insert({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),             -- прокрутка документации вверх
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),              -- прокрутка документации вниз
+        ["<C-Space>"] = cmp.mapping.complete(),              -- вручную вызвать меню
+        ["<C-e>"] = cmp.mapping.abort(),                     -- закрыть меню
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),   -- подтвердить выбор
         ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            else
-                fallback()
-            end
-        end, {"i", "s"}),
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
         ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            else
-                fallback()
-            end
-        end, {"i", "s"})
-    }),
-    sources = cmp.config.sources({
-        {name = 'nvim_lsp'}, {name = 'vsnip'} -- For vsnip users.
-    }, {{name = 'buffer'}, {name = 'nvim_lsp_signature_help'}})
-})
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
 
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-        {name = 'cmp_git'} -- You can specify the `cmp_git` source if you were installed it.
-    }, {{name = 'buffer'}})
-})
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },     -- язык сервера (LSP)
+        { name = "luasnip" },      -- сниппеты
+        { name = "buffer" },       -- текущий буфер
+        { name = "path" },         -- пути к файлам
+      }, {
+        { name = "nvim_lsp_signature_help" },  -- подсказки по сигнатурам функций
+      }),
+    })
 
--- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({'/', '?'}, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {{name = 'buffer'}}
-})
+    -- Отдельная настройка для коммитов Git
+    cmp.setup.filetype("gitcommit", {
+      sources = cmp.config.sources({
+        { name = "cmp_git" },       -- предложения Git
+      }, {
+        { name = "buffer" },        -- + текущий буфер
+      }),
+    })
 
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({{name = 'path'}}, {{name = 'cmdline'}})
-})
+    -- Настройки автодополнения в командной строке `/` и `?`
+    cmp.setup.cmdline({ "/", "?" }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },        -- предложения из буфера
+      },
+    })
 
--- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-require('lspconfig')['tsserver'].setup {capabilities = capabilities}
+    -- Настройки автодополнения в командной строке `:`
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },          -- предложения путей
+      }, {
+        { name = "cmdline" },       -- команды Vim
+      }),
+    })
+
+    -- Подключение LSP-сервера `tsserver` с возможностью автодополнения
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    require("lspconfig").tsserver.setup({
+      capabilities = capabilities
+    })
+  end,
+}
